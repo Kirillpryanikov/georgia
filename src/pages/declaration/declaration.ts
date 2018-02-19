c8644392d6f658e810b8dc20a3eb17e1dc8ad549
AndreyKarpenko <andreykarpenkoua@gmail.com>
2018-02-19 09:24:55 +0200

    create remove product popup

--- src/pages/declaration/declaration.ts
+++ src/pages/declaration/declaration.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {IonicPage, ModalController, NavController, NavParams} from 'ionic-angular';
 import {AddProductPopups} from "@shared/popups/add-product-popup-component/add-product-popups";
+import {WarningPopups} from "@shared/popups/warning-popup-component/warning-popups";
 
 /**
  * Generated class for the DeclarationPage page.
@@ -8,6 +9,10 @@ import {AddProductPopups} from "@shared/popups/add-product-popup-component/add-p
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
+const notice = {
+  rm: 'Please confirm you would like to remove the product',
+
+};
 
 @IonicPage({
   name: 'declaration-page'
@@ -29,6 +34,11 @@ export class DeclarationPage {
 
   addProduct() {
     const modal = this.modalController.create(AddProductPopups);
+    modal.present();
+  }
+
+  removeProduct() {
+    const modal = this.modalController.create(WarningPopups,{notice:notice.rm});
     modal.present();
   }
 }
