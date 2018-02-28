0fa3f1185c9d1c05205d24b3b39c9772e613f254
AndreyKarpenko <andreykarpenkoua@gmail.com>
2018-02-28 12:54:00 +0200

    fix in received

--- src/pages/received/received.ts
+++ src/pages/received/received.ts
@@ -1,8 +1,10 @@
 import {Component} from '@angular/core';
 import {IonicPage, ModalController, NavController, NavParams} from 'ionic-angular';
+
 import {ScriptMainService} from "@core/script.data/script.main.service";
 import {CommentPopups} from "@shared/popups/comment-popup-component/comment-popups";
 import {ReceivedService} from "@core/services";
+
 import {Observable} from "rxjs/Observable";
 import {Subject} from "rxjs/Subject";
 import {debounceTime} from 'rxjs/operators';
@@ -22,8 +24,8 @@ import {debounceTime} from 'rxjs/operators';
   templateUrl: 'received.html',
 })
 export class ReceivedPage {
-  private sessionId = '707d235b00280e693eab0496acb2690d';
-  private listReceived = [];
+  private sessionId: string = '707d235b00280e693eab0496acb2690d';
+  private listReceived: Array<any> = [];
   private subject = new Subject<any>();
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
@@ -55,9 +57,18 @@ export class ReceivedPage {
   getReceived(): Observable<any> {
     this.receivedService.getReceived('getReceived', {sessionId: this.sessionId}).subscribe(data => {
       this.listReceived = data.message.received;
+      for(let i in this.listReceived){
+        let weight_kg = 0;
+        let weight_lbs = 0;
+        for(let j in this.listReceived[i].trackings){
+          weight_kg+=parseFloat(this.listReceived[i].trackings[j].weight_kg);
+          weight_lbs+=parseFloat(this.listReceived[i].trackings[j].weight_lbs);
+        }
+        this.listReceived[i].weight_kg = Math.round(weight_kg * 10) / 10;
+        this.listReceived[i].weight_lbs = Math.round(weight_lbs * 10) / 10;
+      }
       this.subject.next();
     });
     return this.subject.asObservable();
   }
 }
-
