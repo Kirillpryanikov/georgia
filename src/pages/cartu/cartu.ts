3def8aff51a8565fb266bc4a416a0ff61bdebd9e
Andrey Karpenko <andreykarpenkoua@gmail.com>
2018-03-05 20:42:39 +0200

    'fix'

--- src/pages/cartu/cartu.ts
+++ src/pages/cartu/cartu.ts
@@ -3,7 +3,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ScriptMainService } from "@core/script.data/script.main.service";
 import { CartuService } from "@core/services";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import { map } from "rxjs/operators";
 
 /**
  * Generated class for the CartuPage page.
@@ -21,42 +20,35 @@ import { map } from "rxjs/operators";
 })
 export class CartuPage implements OnInit, OnDestroy{
 
-  protected form: FormGroup;
-  private data = '&PurchaseAmt=1.00&PurchaseDesc=' + 'U00540T1520259438' + '&CountryCode=268&CurrencyCode=981&MerchantName=Usa2Georgia&MerchantURL=https%3A%2F%2Fwww.usa2georgia.com%2Fshipping_new%2Fawaiting&MerchantCity=Tbilisi&MerchantID=000000008000308-00000001&xDDDSProxy.Language=01&cartupayment=Payment&';
+  private body = new URLSearchParams();
   private amount;
   private sessionId = '707d235b00280e693eab0496acb2690d';
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public mainService: ScriptMainService,
-              private cartuService: CartuService,
-              private fb: FormBuilder) {
+              private cartuService: CartuService) {
   }
 
   ngOnInit() {
-    // this.initForm();
-  }
 
-  // initForm() {
-  //   this.form = this.fb.group({
-  //     PurchaseAmt: ['12.45'],
-  //     PurchaseDesc: ['U00540T1520259438'],
-  //     CountryCode: ['268'],
-  //     CurrencyCode: ['981'],
-  //     MerchantName:Usa2Georgia
-  //     MerchantURL:https://www.usa2georgia.com/shipping_new/awaiting
-  //   MerchantCity:Tbilisi
-  //   MerchantID:000000008000308-00000001
-  //   ['xDDDSProxy.Language']:['01']
-  //   })
-  // }
+  }
 
   getCartuDescriptor() {
     this.cartuService.getCartuDescriptor('getCartuDescriptor', {sessionId: this.sessionId})
       .subscribe(data => {
-        this.cartuService.pay(this.data)
+        this.body.append('PurchaseAmt', this.amount);
+        this.body.append('PurchaseDesc', data.message.descriptor);
+        this.body.append('CountryCode', '268');
+        this.body.append('CurrencyCode', '981');
+        this.body.append('MerchantName', 'Usa2Georgia');
+        this.body.append('MerchantURL', 'https://www.usa2georgia.com/shipping_new/awaiting');
+        this.body.append('MerchantCity', 'Tbilisi');
+        this.body.append('MerchantID', '000000008000308-00000001');
+        this.body.append('xDDDSProxy.Language', '01');
+        this.cartuService.pay(this.body.toString())
           .subscribe(data => {
             console.log(data);
-          })
+        })
     })
   }
 
