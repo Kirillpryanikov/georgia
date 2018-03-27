02fb32b31c0a85bff9e2f411acdd74c9ba8229df
smartit <smartit@MacBook-Pro-smartit.local>
2018-03-27 15:28:23 +0300

    fix lang

--- src/@core/script.data/script.main.service.ts
+++ src/@core/script.data/script.main.service.ts
@@ -12,8 +12,43 @@ export class ScriptMainService {
 
   dropdown() {
     const that = this;
-    $(document).on('click', '[data-show-element]', function () {
+    $(document).on('touchstart', '[data-show-element]', function () {
       $('.' + $(this).data('show-element')).toggleClass('u2g-show');
+
+      $(this).addClass('u2g-showed');
+
+      $('.u2g-overlay').css({'display' : 'block'});
+
+      $('body').addClass('overflow');
+
+      if ($(this).hasClass('u2g-menu-toggle')) {
+        $(this).addClass('u2g-menu-toggle--open');
+
+        $('.u2g-page-wrapper').addClass('u2g-page-wrapper--right');
+      }
+    });
+
+    $(document).on('touchstart', function (e) {
+
+      var showItem = $('.u2g-show');
+
+      if(showItem.length > 0) {
+
+        if($(e.target).hasClass('u2g-showed')) {
+          return;
+        }
+
+        if (!showItem.is(e.target) && showItem.has(e.target).length === 0) {
+          that.hideDropdown();
+        }
+      }
+    });
+  }
+
+  dropdownLang() {
+    const that = this;
+    $(document).on('click', '[data-show-elem]', function () {
+      $('.' + $(this).data('show-elem')).toggleClass('u2g-show');
 
       $(this).addClass('u2g-showed');
 
