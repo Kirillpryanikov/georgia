0fe4a932c84cc6f4b21bf180a05ab541c5b66564
smartit <smartit@MacBook-Pro-smartit.local>
2018-05-31 15:41:18 +0300

    fix

--- src/pages/header/header.ts
+++ src/pages/header/header.ts
@@ -108,7 +108,20 @@ export class HeaderPage implements OnInit, OnDestroy{
       this.subscription = this.headerService.getInfo('getInfo', {sessionId: this.sessionId}).subscribe(data => {
         this.lang = data.message.profile.panel_language || 'en';
         this.getBranchSelection(data);
-        localStorage.setItem('lang', data.message.profile.panel_language || 'en')
+        if(data.message.status === "EXPIRED"){
+          this.nativeStorage.remove('sessionId');
+          this.navCtrl.setRoot('authorization-page');
+          return;
+        }
+        this.getBranchSelection(data);
+        this.user.userName = data.message.profile.first_name + ' ' + data.message.profile.last_name;
+        this.user.email = data.message.profile.email;
+        this.user.userCode = data.message.profile.suite;
+        this.user.userBalance = data.message.profile.balance;
+        this.lang = data.message.profile.panel_language || 'en';
+        this.translate.use(this.lang);
+        localStorage.setItem('lang', data.message.profile.panel_language || 'en');
+        this.subscription.unsubscribe();
       })
     } else {
       this.subscription = this.headerService.getInfo('getInfo', {sessionId: this.sessionId}).subscribe(data => {
