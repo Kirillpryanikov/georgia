e3f4bee89031d8d7f6b70a1cdeef8d51481397c7
Kirillpryanikov <Kirillpryanikov@gmail.com>
2018-08-14 18:16:28 +0300

    fix pin page

--- src/pages/pin/pin.ts
+++ src/pages/pin/pin.ts
@@ -1,54 +1,79 @@
-import {Component, OnInit} from '@angular/core';
-import {IonicPage, LoadingController, NavController, NavParams} from 'ionic-angular';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { Component } from '@angular/core';
+import {IonicPage, LoadingController, ModalController, NavController, NavParams} from 'ionic-angular';
 import {NativeStorage} from "@ionic-native/native-storage";
-import {AuthorizationService} from "@core/services";
 import {Subscription} from "rxjs/Subscription";
+import {AuthorizationService} from "@core/services";
 import {TranslateService} from "@ngx-translate/core";
-import {ScriptRegisterService} from "@core/script.data/script.register.service";
-
-/**
- * Generated class for the PinPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import {ErrorPopups} from "@shared/popups/error-popup-component/error-popups";
 
 @IonicPage({
-  name: "page-pin"
+  name: 'page-pin'
 })
 @Component({
   selector: 'page-pin',
   templateUrl: 'pin.html',
 })
-export class PinPage implements OnInit{
-
-  private form: FormGroup;
-  private lang: string = 'en';
-  private data;
-  private msg: string;
-  private key;
+export class PinPage {
+  private count: number = 0;
+  private pin: string = '';
   private load;
+  private data;
+  private key;
+  public lang: string = '';
   private subscription: Subscription;
 
   constructor(public navCtrl: NavController,
-              private translate: TranslateService,
               public navParams: NavParams,
-              private registerService: ScriptRegisterService,
-              private fb: FormBuilder,
-              private nativeStorage: NativeStorage,
-              private loadingCtrl: LoadingController,
-              private authService: AuthorizationService) {
+              public loadingCtrl: LoadingController,
+              public nativeStorage: NativeStorage,
+              public modalCtrl: ModalController,
+              public translate: TranslateService,
+              public authService: AuthorizationService) {
+    this.lang = this.translate.currentLang;
   }
 
-  ngOnInit() {
-    this.initForm();
-  }
-
-  changeLanguage(language: string) {
-    this.registerService.hideDropdown();
-    this.lang = language;
-    this.translate.use(language);
+  writePin(key){
+    switch (key){
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+      case 7:
+      case 8:
+      case 9:
+      case 0:
+        const a = document.getElementById(`_${this.count}`);
+        if(a){
+          this.pin+=key;
+          a.style.color = '#666666';
+          this.count++;
+          if(this.pin.length === 4){
+            this.login();
+          }
+        }
+        break;
+      case '_CLEAR':
+        const b: any = document.getElementsByClassName('input_pin');
+        if(b){
+          this.pin = '';
+          this.count = 0;
+          for(let i = 0; i < b.length; i++){
+            b[i].style.color = 'rgba(0,0,0,0)';
+          }
+        }
+        break;
+      case 'remove':
+        if(this.count > 0)
+          this.count--;
+        const c = document.getElementById(`_${this.count}`);
+        if(c){
+          this.pin = this.pin.substring(0, this.pin.length - 1);
+          c.style.color = 'rgba(0,0,0,0)';
+        }
+        break;
+    }
   }
 
   login() {
@@ -58,7 +83,7 @@ export class PinPage implements OnInit{
     this.load.present();
     this.nativeStorage.getItem('pin')
       .then(res => {
-        if(res === this.form.value.pin){
+        if(res === this.pin){
           this.nativeStorage.getItem('hashKey')
             .then(res => {
               this.key = res;
@@ -66,7 +91,7 @@ export class PinPage implements OnInit{
                 secret: '6LcbGCsUAAATUM-mRB1xmIGEAbaSfebzeUlPpsuZ',
                 key: this.key,
                 language: this.lang,
-                remember: this.form.value.checkbox
+                remember: true
               };
               this.subscription = this.authService.keyLogin('keyLogin', this.data).subscribe(data => {
                 this.nativeStorage.setItem('sessionId', data.message.session_id);
@@ -77,20 +102,20 @@ export class PinPage implements OnInit{
               })
             });
         } else {
-          this.msg = 'Wrong PIN Code';
+          const b: any = document.getElementsByClassName('input_pin');
+          if(b){
+            this.pin = '';
+            this.count = 0;
+            for(let i = 0; i < b.length; i++){
+              b[i].style.color = 'rgba(0,0,0,0)';
+            }
+          }
+          const modal = this.modalCtrl.create(ErrorPopups, {notice: "_YOUR_PIN_CODE_NOT_CONFIRMED"});
+          modal.present();
           this.load.dismiss();
         }
       });
   }
 
-  initForm() {
-    this.form = this.fb.group({
-      pin: ['', Validators.compose([
-        Validators.maxLength(4),
-        Validators.pattern(/\d{4}/),
-        Validators.required
-      ])],
-      checkbox: false
-    })
-  }
 }
+
